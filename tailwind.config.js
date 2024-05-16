75c5cd6ed89a2a1593f7e785bcafc998037a6f3e
tanjim00 <tanjim.ac00@gmail.com>
2024-05-16 14:24:05 +0600

    implement game logic with history tracking

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', 
+    './components/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
